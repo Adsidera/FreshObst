5eb1caeadac1e86b349b8006a55590cbee8ce827
adsidera <annamaria.costalonga@gmail.com>
2015-12-04 21:52:29 +0100

    dinuovo

---
+++ db/migrate/20151203214601_delete_product.rb
@@ -0,0 +1,4 @@
+class DeleteProduct < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151129155923) do
+ActiveRecord::Schema.define(version: 20151203214601) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
