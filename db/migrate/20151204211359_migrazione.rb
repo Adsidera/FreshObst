5952bba0a5698c99706ed1cfd684214fe1fafb1e
adsidera <annamaria.costalonga@gmail.com>
2015-12-04 22:14:47 +0100

    prova

---
+++ db/migrate/20151204211359_migrazione.rb
@@ -0,0 +1,4 @@
+class Migrazione < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203214601) do
+ActiveRecord::Schema.define(version: 20151204211359) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
