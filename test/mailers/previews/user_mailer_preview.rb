d2d0c282ffec9331dcdd112b71534963be49e730
adsidera <annamaria.costalonga@gmail.com>
2016-01-31 14:09:06 +0100

    layoutmigliorato

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,6 +1,4 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-    def contact_form 
-        UserMailer.contact_form("john@example.com", "John", "Test Email" )
-    end
+   
 end
