b0df1bc6b7c01f8d61656aaf38910261d0e5957e
adsidera <annamaria.costalonga@gmail.com>
2016-01-31 15:32:42 +0100

    riprova

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,6 @@ ActionMailer::Base.smtp_settings = {
     :port           => '25',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD']',
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com'
 }
