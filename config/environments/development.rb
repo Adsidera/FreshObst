ae20a8bbf990e47eb0bd9df61a05c708d63d4223
adsidera <annamaria.costalonga@gmail.com>
2016-02-02 17:51:49 +0100

    changedassetsprefixindevelopmentrb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,5 @@ Rails.application.configure do
     config.action_mailer.default_url_options = {:host => 'localhost:3000'}
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
+    config.assets.prefix = '/dev/assets'
 end
