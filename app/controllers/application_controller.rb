b58f70710413f01014d45d4408bf7da8f95c7aa3
adsidera <annamaria.costalonga@gmail.com>
2016-05-19 14:08:32 +0200

    changedloginlayout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,33 @@ class ApplicationController < ActionController::Base
         redirect_to main_app.root_url, :alert => exception.message
     end
 
+    
 protected
+    
+after_filter :store_location
+    
+def store_location
+  # store last url - this is needed for post-login redirect to whatever the user last visited.
+  return unless request.get? 
+  if (request.path != "/users/sign_in" &&
+      request.path != "/users/sign_up" &&
+      request.path != "/users/password/new" &&
+      request.path != "/users/password/edit" &&
+      request.path != "/users/confirmation" &&
+      request.path != "/users/sign_out" &&
+      !request.xhr?) # don't store ajax calls
+    session[:previous_url] = request.fullpath 
+  end
+end
 
+    def after_sign_in_path_for(resource)
+  session[:previous_url] || root_path
+end   
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:first_name, :last_name, :email) }
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation) }
   	devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation)}
   end
+ 
+    
 end
